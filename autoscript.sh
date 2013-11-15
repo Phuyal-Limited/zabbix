41536ff515a2e14c5e1c2d24853a8ad91c1bbdf7
Bikram Adhikari <salik.adhikari@gmail.com>
2013-11-15 03:05:48 -0700

    Added the automation script

---
+++ autoscript.sh
@@ -0,0 +1,10 @@
+groupadd zabbix
+useradd -g zabbix zabbix
+wget http://kaz.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/2.2.0/zabbix-2.2.0.tar.gz
+tar -xvf zabbix*
+cd zabbix-2.2.0
+./configure --enable-agent
+sudo make install
+wget https://raw.github.com/anjani-phuyal/zabbix/master/zabbix_agentd.conf
+rm /usr/local/etc/zabbix_agentd.conf
+sudo cp zabbix_agentd.conf /usr/local/etc/zabbix_agentd.conf
